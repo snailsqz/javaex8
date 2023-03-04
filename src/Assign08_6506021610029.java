31df7cc2f92b684126c85da07799175b2c7edffe
snails <snailsqz@gmail.com>
2023-03-04 23:43:20 +0700

    Hello

---
+++ src/Assign08_6506021610029.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Assign08_6506021610029 extends JFrame implements ActionListener{
+    private Button Play,Stop,toRight,toLeft,toTop,toDown,Exit;
+    private Button SpeedUp,SpeedDown,CBlack,CRed,CGreen,CBlue;
+    private JPanel panelG;
+    private Ball mBall;
+
+    public Assign08_6506021610029(){
+        super("Assignment #08 Control Ball to Animation");
+        String Hello;
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
